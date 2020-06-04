984c3b044b176156404a2aabeac7c7913dfdb761
avnoto <alexandravnoto@gmail.com>
2020-06-04 12:16:30 -0700

    description prompt added

--- index.js
+++ index.js
@@ -2,33 +2,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const axios = require('axios');
 
-//const questions = [];
-
 //function writeToFile(fileName, data) {}
 
 inquirer
-  .prompt({
-    message: 'Enter your GitHub username',
-    name: 'username',
-  })
+  .prompt([
+    {
+      message: 'Enter your GitHub username',
+      name: 'username',
+    },
+  ])
   .then(function ({ username }) {
     const queryURL = `https://api.github.com/users/${username}/repos?per_page=100`;
     axios.get(queryURL).then(function (response) {
       const repos = response.data;
       const repoNames = repos.map((repo) => repo.name);
-      const repoNamesAsText = repoNames.join('\n'); //converts strings in array as text
-      console.log(repoNamesAsText);
+      inquirer.prompt([
+        {
+          message: 'Which repository would you like to make the README.md for?',
+          type: 'checkbox',
+          name: 'githubRepos',
+          choices: function () {
+            return repoNames;
+          },
+        },
+        {
+          message:
+            'Is the name of your project the same as your repository name?',
+          type: 'confirm',
+          default: true,
+          name: 'projectNameSame',
+        },
+        {
+          message: 'Enter a different title for your project.',
+          type: 'input',
+          name: 'projectNameGithub',
+          when: function (data) {
+            return data.projectNameSame === false;
+          },
+        },
+        {
+          message: 'Enter a description for your project.',
+          type: 'input',
+          name: 'projectDescription',
+          when: function (data) {
+            return (
+              data.projectNameGithub === false || data.projectNameSame === true
+            );
+          },
+        },
+      ]);
     });
-  })
-  .then(function () {
-    inquirer.prompt([
-      {
-        type: 'confirm',
-        message:
-          'Would you like to create a README for one of these repositories?',
-
-        name: 'repoNames',
-        default: false,
-      },
-    ]);
   });
